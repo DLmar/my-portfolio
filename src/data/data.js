498405a1ffb0fb5f66c72e2d8955abff7fafa0a7
DLmar <dlmar2code@gmail.com>
2023-04-07 12:33:41 +0300

    ;

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import Qi from '../assets/projects/img_2.png'
 import Pb from '../assets/projects/img.png'
+import Pk from '../assets/projects/img_3.png'
 import Mv from '../assets/projects/img_1.png'
 
 
@@ -12,14 +13,19 @@ export const data=[
         live:"https://simple-quiz-eta.vercel.app/",
     },{
         id:2,
-        name:"Using Tailwindcss with React JS",
+        name:"Markup on Tailwindcss in React JS",
         image:Pb,
         github:"https://github.com/DLmar/pbit",
         live:"https://pbit.vercel.app/",
-    },
-    {
-        id:2,
-        name:"React TS with Redux",
+    }, {
+        id:3,
+        name:"React JS with Redux and Material UI",
+        image:Pk,
+        github:"https://github.com/DLmar/pokemonsAPI",
+        live:"https://pokemons-eosin.vercel.app/",
+    }, {
+        id:4,
+        name:"React TS with Redux Toolkit",
         image:Mv,
         github:"https://github.com/DLmar/movie-ts",
         live:"https://movie-ts-phi.vercel.app/",

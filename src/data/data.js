b55de2ffeba6d7a53caa6a92bb51e63cb9d0d119
DLmar <dlmar2code@gmail.com>
2023-04-07 12:51:54 +0300

    ;

--- src/data/data.js
+++ src/data/data.js
@@ -1,37 +1,41 @@
 import Qi from '../assets/projects/img_2.png'
 import Pb from '../assets/projects/img.png'
 import Pk from '../assets/projects/img_3.png'
+import Td from '../assets/projects/img_4.png'
 import Mv from '../assets/projects/img_1.png'
 
 
 export const data=[
     {
         id:1,
+        name:"Todo list on JS",
+        image:Td,
+        github:"https://github.com/DLmar/js-tests/tree/master/todo",
+        live:"https://todo-pi-ashen.vercel.app/",
+    },
+    {
+        id:2,
         name:"Simple quiz on React JS",
         image:Qi,
         github:"https://github.com/DLmar/simple-projects",
         live:"https://simple-quiz-eta.vercel.app/",
-    },{
-        id:2,
+    }, {
+        id:3,
         name:"Markup on Tailwindcss in React JS",
         image:Pb,
         github:"https://github.com/DLmar/pbit",
         live:"https://pbit.vercel.app/",
     }, {
-        id:3,
+        id:4,
         name:"React JS with Redux and Material UI",
         image:Pk,
         github:"https://github.com/DLmar/pokemonsAPI",
         live:"https://pokemons-eosin.vercel.app/",
     }, {
-        id:4,
+        id:5,
         name:"React TS with Redux Toolkit",
         image:Mv,
         github:"https://github.com/DLmar/movie-ts",
         live:"https://movie-ts-phi.vercel.app/",
     },
-
-
-
-
 ]
